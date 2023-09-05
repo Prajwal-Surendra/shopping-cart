ba3978d7b159f42c7e2ebca91489a6eef5bd6bf1
Prajwal <prajwalsurendra7178@gmail.com>
2023-09-05 13:23:33 +0530

    Updated

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shop-title">
+        <h1> Shopping Cart </h1>
+      </div>
+      <div className="products">
+        {" "}
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,31 @@
+import product1 from "./assets/1.jpeg";
+import product2 from "./assets/2.jpeg";
+import product3 from "./assets/3.jpeg";
+import product4 from "./assets/4.jpeg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "iPhone 14 Pro",
+    price: "116900",
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "MacBook Pro 2022",
+    price: "149900",
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Realme GT 5G(128 GB)",
+    price: "30999",
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "boAt Rockerz 551 ANC",
+    price: "3499",
+    productImage: product4,
+  },
+];
